a9d0213874e820b8eacfd543709a7226f44684e7
edu <alejandro.902019@hotmail.com>
2024-03-04 01:03:07 +0100

    last

--- node/App.js
+++ node/App.js
@@ -1,31 +1,20 @@
 import express from "express";
+
 import cors from "cors";
 import db from "./database/db.js";
 import HomeRoutes from "./routes/HomeRoutes.js";
 import userRoutes from "./routes/UserRoutes.js";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import multer from "multer";
-
-// mongoose.connect("mongodb://127.0.0.1:27017/Universal", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// mongoose.connect("mongodb+srv://alejandro902019:456@cluster1.eoszury.mongodb.net/Universal?retryWrites=true&w=majority&appName=Cluster1", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose.Promise = global.Promise;
 
 const app = express();
+const port = process.env.PORT || 8000; // Definir el puerto de esta manera
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/Home", HomeRoutes);
 app.use("/Users", userRoutes);
 
-app.listen(8000, () => {
-  console.log("Server UP running in http://localhost:8000/");
+app.listen(port, () => { // Usar la variable 'port' aquí
+  console.log(`Server UP running on http://localhost:${port}/`); // Usar 'port' para mostrar el puerto dinámicamente
 });
